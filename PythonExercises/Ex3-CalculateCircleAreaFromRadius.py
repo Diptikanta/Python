a9522ec6d1cd976138a3770e0473178f854d0161
Diptikanta <dipsmac@Diptikantas-MacBook-Pro.local>
2020-04-06 01:07:27 -0400

    Initial Commit

---
+++ PythonExercises/Ex3-CalculateCircleAreaFromRadius.py
@@ -0,0 +1,11 @@
+
+#This exercise will ask the user to enter the radius so that it can calculate the area of the circle
+#This is the first exercise which deals with the user inputs.
+
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+
+area =  math.pi * radius * radius
+print("Radius is ", radius)
+print("Area is ", area)
---
+++ PythonExercises/Ex4-ReverseUserinputStrings.py
@@ -0,0 +1,11 @@
+
+https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+#This exercise takes the user name as input and displays it in the reverse order
+
+import string
+
+
+firstName = input("Enter your first name: ")
+lastName  = input("Enter your last name: ")
+
