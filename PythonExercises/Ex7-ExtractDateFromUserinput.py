c2bf5d4e102544e36f73cef44c4849965802127b
Diptikanta <dip.sandhicse@gmail.com>
2020-04-09 00:22:56 -0400

    Exercise-7 completed
    Signed-off-by: Diptikanta <dip.sandhicse@gmail.com>

---
+++ PythonExercises/Ex7-ExtractDateFromUserinput.py
@@ -0,0 +1,19 @@
+
+
+#Write a Python program to display the examination schedule. (extract the date from exam_st_date). Go to the editor
+#exam_st_date = (11, 12, 2014)
+#Sample Output : The examination will start from : 11 / 12 / 2014
+
+examStartDate: str = input("Enter the exam start date: ")
+examStartDate = examStartDate.replace("(","")
+examStartDate = examStartDate.replace(")","")
+examStartDate = examStartDate.split(",")
+
+examMonth   = examStartDate[0]
+examDate    = examStartDate[1]
+examyear    = examStartDate[2]
+
+print("The exam date is: " + examMonth + "/" + examDate + "/" + examyear)
+
+
+## Comment: I am sure there is a better and more optimized way of solving this.
